18594501e45e442cf9e0688bd623e4b771d5d721
xugf <345632369@qq.com>
2017-08-17 14:22:04 +0800

    udp完整通信demo

--- socket/src/socket/UDPClient.java
+++ socket/src/socket/UDPClient.java
@@ -5,13 +5,31 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import javax.print.DocFlavor.STRING;
+import javax.xml.crypto.Data;
+
+import org.omg.Messaging.SyncScopeHelper;
+
 public class UDPClient {
 	public static void main(String[] args) throws IOException {
+		/**
+		 * 客户端发送
+		 */
 		InetAddress address = InetAddress.getByName("localhost");
 		int port = 8888;
 		byte[] data = "user:admin;password:admin123;".getBytes();
 		DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
 		DatagramSocket socket = new DatagramSocket();
 		socket.send(packet);
+		
+		/**
+		 * 客户端接收
+		 */
+		byte[] response = new byte[1024];
+		DatagramPacket resopnsePacket = new DatagramPacket(response, response.length);
+		socket.receive(resopnsePacket);
+		String responseString = new String(response, 0, resopnsePacket.getLength());
+		System.out.println(responseString);
+		socket.close();
 	}
 }
--- socket/src/socket/UDPServer.java
+++ socket/src/socket/UDPServer.java
@@ -3,16 +3,31 @@ package socket;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.sql.DatabaseMetaData;
 
 public class UDPServer {
 	public static void main(String[] args) throws IOException {
+		
+		/**
+		 * 服务器接收
+		 */
 		DatagramSocket socket = new DatagramSocket(8888);
 		byte[] data = new byte[1024];
 		DatagramPacket packet = new DatagramPacket(data, data.length);
 		socket.receive(packet);
 		String info = new String(data,0,packet.getLength());
 		System.out.println("我是服务器，客户端说："+info);
+		/**
+		 * 服务器响应
+		 */
+		
+		InetAddress clInetAddress  = packet.getAddress();
+		int clinetPort = packet.getPort();
+		byte[] response = "欢迎你".getBytes();
+		DatagramPacket responsePacket = new DatagramPacket(response, response.length, clInetAddress, clinetPort);
+		socket.send(responsePacket);
+//		socket.close();
 	}
 }
